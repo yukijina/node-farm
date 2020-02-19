dc39dc45833d457a8d35a293c7b3b03ee02696d4
yukijina <yukijina@gmail.com>
2020-02-18 18:10:06 -0800

    replace html contents with json data

--- index.js
+++ index.js
@@ -34,8 +34,24 @@ const url = require('url');
 
 /////////////// SERVER
 ///// Create a server and listen
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
 
-//synchronous version - we only call once the program starts
+  if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  return output;
+}
+
+//synchronous version - we only call once when the program starts
+const overview = fs.readFileSync(`${__dirname}/templates/overview.html`, 'utf-8');
+const product = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-8');
+const card = fs.readFileSync(`${__dirname}/templates/card.html`, 'utf-8');
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
@@ -44,10 +60,21 @@ const server = http.createServer((req,res) => {
 
   const pathName = req.url;
 
+  // Overview page
   if (pathName === '/' || pathName === '/overview') {
-    res.end('This is the Overview!!'); 
+    res.writeHead(200, {'Content-type': 'text/html'});
+
+    //map returns array so we change it to string with "join"
+    const cardsHtml = dataObj.map(el => replaceTemplate(card, el)).join('');
+    const output = overview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end(output);
+
+  // Product page
   } else if (pathName === '/product') {
-    res.end('This is Product!!!')
+    res.writeHead(200, {'Content-type': 'text/html'});
+    res.end(product)
+
+  // API
   } else if (pathName === '/api') {
     //fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
       //const productData = JSON.parse(data);
@@ -58,6 +85,7 @@ const server = http.createServer((req,res) => {
 
     res.writeHead(200, {'Content-type': 'application/json'});
     res.end(data)
+  // Not found
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
